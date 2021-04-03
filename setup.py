fba96c8285e37294623c17b959b9f4ad96ce4ab6
Lars Schwegmann <schwegmannlars@gmail.com>
2021-04-03 13:06:47 +0200

    add clint dep

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    install_requires=["Click"],
+    install_requires=["Click", "clint"],
     entry_points={
         "console_scripts": [
             "pyawsbackup = pyawsbackup:cli"
